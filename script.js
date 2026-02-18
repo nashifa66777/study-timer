4d7e057c71018f8fc357ae35e345f57627ad5ff2
nashifa66777 <nashifahnavanda@gmail.com>
2026-02-18 14:20:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -123,10 +123,10 @@ function changeSound() {
   let choice = document.getElementById("soundSelect").value;
 
   const sounds = {
-    disneypiano: "sound/disneypiano.mp3",
-    lofisound: "sound/lofisound.mp3",
-    relaxcalm: "sound/relaxcalm.mp3",
-    taylorswiftpiano: "sound/taylorswiftpiano.mp3"
+    disneypiano: "sha256:79cdb1010a15f0c491bf6395f9ad5284f959635808f5585eedbdb01f784cac5d",
+    lofisound: "sha256:7a3ab7b6e785a1c3562c56ff5769593b53119172bb9d86a10f50a1aa010a4bd3",
+    relaxcalm: "sha256:c6b8846e45e897ba0df077128c15db59a5487d4fd13b29ec5a4dcfa82eb48e39",
+    taylorswiftpiano: "sha256:725bcba4ece54ef8103b68a8aec344fa071f4e66891be6a46b92c4211ef7d21e"
   };
 
   if (choice === "off") {
@@ -421,3 +421,4 @@ window.onload = () => {
   updateChart();
   showPage("dashboard");
 };
+
